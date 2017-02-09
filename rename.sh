1b7e2b0179218b37658735b46e3e662588052eb2
Kai Davenport <kaiyadavenport@gmail.com>
2017-02-09 13:37:39 +0000

    wrap move into a function

--- rename.sh
+++ rename.sh
@@ -5,9 +5,16 @@ set -e
 export PATH_TO_FILES=${PATH_TO_FILES:="$1"}
 export FILE_TYPE=${FILE_TYPE:="$2"}
 
-a=1
-for i in $PATH_TO_FILES/*.$FILE_TYPE; do
-  new=$(printf "%04d.$FILE_TYPE" "$a") #04 pad to length of 4
-  mv -i -- "$i" "$new"
-  let a=a+1
-done
+function move_files() {
+  local filepath="${1}"
+  local filetype="${2}"
+  local imageindex=1
+
+  for i in ${filepath}/*.${filetype}; do
+    new=$(printf "%04d.$FILE_TYPE" "$imageindex") #04 pad to length of 4
+    echo mv -i -- "$i" "$new"
+    imageindex=$((imageindex+1))
+  done
+}
+
+move_files "${PATH_TO_FILES}" "${FILE_TYPE}"
